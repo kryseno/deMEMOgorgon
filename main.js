ced0a0013d74f7b0fcb033b638ed921095f52292
kryseno <kryseno@gmail.com>
2017-12-07 23:50:10 -0800

    [updated] removed some early console.logs. rearranged if/else statement. 1st & 2nd cards clicked recorded correctly. need to figure out how to place back of card on top

--- main.js
+++ main.js
@@ -3,22 +3,20 @@ let second_card_clicked = null;
 
 $(document).ready(initializeApp);
 
-function initializeApp (){
+function initializeApp(){
     console.log('app initialized');
     $(".card").click(handleCardClick);
 }
 
-function handleCardClick (){
-    console.log('card clicked', this);
+function handleCardClick(){
     $(this).find('.front').removeClass('remove');
 
     if(first_card_clicked === null){
         first_card_clicked = this;
-        console.log('first card clicked this: ', this);
+        console.log('first card: ',first_card_clicked);
     } else {
         second_card_clicked = this;
-        console.log('second card clicked this: ', this);
-        
+        console.log('second card: ',second_card_clicked);
         if($(first_card_clicked).find("img").attr("src") === $(second_card_clicked).find("img").attr("src")){
             console.log('issa match!');
             first_card_clicked = null;
@@ -30,4 +28,3 @@ function handleCardClick (){
         }
     }
 }
-
